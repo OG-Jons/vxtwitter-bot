65d419e39935be28cc11edba08e514923bcebbdc
OG-Jons <jonas@marschall.pro>
2023-01-03 11:47:14 +0100

    :sparkles: added mobile twitter support

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 const {Client, Events, GatewayIntentBits} = require('discord.js');
 
-const twitterRegex = /https:\/\/twitter\.com\/[a-zA-Z0-9_]{1,15}\/status\/[0-9]+/
+const twitterRegex = /https:\/\/(mobile\.)?twitter\.com\/[a-zA-Z0-9_]{1,15}\/status\/[0-9]+/
 
 // Create a new client instance
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],});
@@ -11,6 +11,9 @@ const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}!`);
+    client.user.setActivity({
+        name: ""
+    })
 });
 
 client.on(Events.MessageCreate, async message => {
@@ -19,8 +22,9 @@ client.on(Events.MessageCreate, async message => {
         await message.suppressEmbeds(true);
         const matched = content.match(twitterRegex)[0]
         const [urlPart, identifier] = matched.split('twitter.com')
+        console.log(`Link replaced: ${matched}`)
         await message.reply({
-            content: `Twitter link replaced with VXTwitter: ${urlPart}vxtwitter.com${identifier}`,
+            content: `Twitter link replaced with VXTwitter: https://vxtwitter.com${identifier}`,
             allowedMentions: {
                 repliedUser: false
             }
